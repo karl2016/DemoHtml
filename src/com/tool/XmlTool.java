57ab5706ac4aaaf9851f069169972617fd6081d6
karl2016 <karl.wang.work@foxmail.com>
2017-03-03 23:13:53 +0800

    refactor：use xpah to parse xml text

--- src/com/tool/XmlTool.java
+++ src/com/tool/XmlTool.java
@@ -5,6 +5,9 @@ import java.io.InputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -13,10 +16,9 @@ import org.w3c.dom.NodeList;
 
 import android.util.Log;
 
-
 public class XmlTool {
 	static final String TAG = "XmlTool";
-	
+
 	/*
 	 * example xml text
 	 * 
@@ -24,108 +26,95 @@ public class XmlTool {
 	 * type="padweb">http://www.langjingyuan.com/padweb/getversion.php</url>
 	 * </uri>
 	 */
+
 	public static String readRealUrl(String xmlText) throws Exception {
-		InputStream inStream;
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		Document doc;
+		XPath xpath;
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setValidating(false);
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		doc = db.parse(new ByteArrayInputStream(xmlText.getBytes()));
 
-		inStream = new ByteArrayInputStream(xmlText.getBytes());
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		Document dom = builder.parse(inStream);
-		inStream.close();
-		Element root = dom.getDocumentElement();
-		NodeList childsNodes = root.getElementsByTagName("url");
-		if (childsNodes.getLength() > 0) {
-			Node node = (Node) childsNodes.item(0); // 判断是否为元素类型
-			if (node.getNodeType() == Node.ELEMENT_NODE) {
-				Element childNode = (Element) node;
-				if ("url".equals(childNode.getNodeName())) {
-					return childNode.getFirstChild().getNodeValue();
-				}
-			}
+		// 创建XPath对象
+		XPathFactory factory = XPathFactory.newInstance();
+		xpath = factory.newXPath();
+
+		Node node = (Node) xpath.evaluate("/uri/url[@type=\"padweb\"]", doc,
+				XPathConstants.NODE);
+
+		if (node == null) {
+			String exception = "url parse failed:" + xmlText;
+			Log.v(TAG, exception);
+			throw new Exception(exception);
 		}
+		Log.v(TAG, node.getTextContent());
 
-		String exception = "url parse failed:"+ xmlText;
-		Log.v(TAG, exception);
-		throw new Exception(exception);
+		return node.getTextContent();
 	}
-/*
- * sample xml text 
-	 <objs>
-		<obj name="padweb_html">
-		<latest ver="1.1.0(demo)" require_apk="1.0.0">
-		http://www.langjingyuan.com/padweb/Demo.v1.1.0(demo).zip
-		</latest>
-		<old ver="1.1.0(demo)" require_apk="1.0.0">
-		http://www.langjingyuan.com/padweb/Demo.v1.1.0(demo).zip
-		</old>
-		<old ver="1.0.0" require_apk="1.0.0">http://www.langjingyuan.com/padweb/Demo.zip</old>
-		</obj>
-		<obj name="padweb_apk">
-		<latest ver="1.0.1">
-		http://www.langjingyuan.com/padweb/padweb_apk_v1.0.1.zip
-		</latest>
-		<old ver="1.0.1">
-		http://www.langjingyuan.com/padweb/padweb_apk_v1.0.1.zip
-		</old>
-		<old ver="1.0.0">
-		http://www.langjingyuan.com/padweb/padweb_apk_v1.0.0.zip
-		</old>
-		</obj>
-		</objs>
 
- * */
+
+	/*
+	 * sample xml text <objs> <obj name="padweb_html"> <latest ver="1.1.0(demo)"
+	 * require_apk="1.0.0">
+	 * http://www.langjingyuan.com/padweb/Demo.v1.1.0(demo).zip </latest> <old
+	 * ver="1.1.0(demo)" require_apk="1.0.0">
+	 * http://www.langjingyuan.com/padweb/Demo.v1.1.0(demo).zip </old> <old
+	 * ver="1.0.0"
+	 * require_apk="1.0.0">http://www.langjingyuan.com/padweb/Demo.zip</old>
+	 * </obj> <obj name="padweb_apk"> <latest ver="1.0.1">
+	 * http://www.langjingyuan.com/padweb/padweb_apk_v1.0.1.zip </latest> <old
+	 * ver="1.0.1"> http://www.langjingyuan.com/padweb/padweb_apk_v1.0.1.zip
+	 * </old> <old ver="1.0.0">
+	 * http://www.langjingyuan.com/padweb/padweb_apk_v1.0.0.zip </old> </obj>
+	 * </objs>
+	 */
 	public static UpdateInfo readUpdateInfo(String xmlText) throws Exception {
-		InputStream inStream = null;
 		UpdateInfo updateInfo = new UpdateInfo();
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		Document doc;
+		XPath xpath;
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setValidating(false);
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		doc = db.parse(new ByteArrayInputStream(xmlText.getBytes()));
 
-		inStream = new ByteArrayInputStream(xmlText.getBytes());
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		Document dom = builder.parse(inStream);
+		// 创建XPath对象
+		XPathFactory factory = XPathFactory.newInstance();
+		xpath = factory.newXPath();
 
-		Element root = dom.getDocumentElement();
-		NodeList objNodes = root.getElementsByTagName("obj");
-		for (int i = 0; i < objNodes.getLength(); i++) {
-			Node objNode = (Node) objNodes.item(i); // 判断是否为元素类型
-			if (objNode.getNodeType() == Node.ELEMENT_NODE) {
-				if ("obj".equals(objNode.getNodeName())) {
+		NodeList nodeList = (NodeList) xpath.evaluate(
+				"/objs/obj[@name=\"padweb_html\"]/latest|/objs/obj[@name=\"padweb_html\"]/old", doc,
+				XPathConstants.NODESET);
+		if (nodeList.getLength() == 0) {
+			String exception = "url parse failed:" + xmlText;
+			Log.v(TAG, exception);
+			throw new Exception(exception);
+		}
+		
 
-					if ("padweb_html".equals(((Element) objNode)
-							.getAttribute("name"))) {
-						NodeList infoList = objNode.getChildNodes();
-						for (int j = 0; j < infoList.getLength(); j++) {
-							Node infoNode = infoList.item(j);
-							if ((objNode.getNodeType() == Node.ELEMENT_NODE)
-									&& ("old".equals(infoNode.getNodeName()) ||
-											("latest".equals(infoNode.getNodeName())))) {
-								updateInfo.addNewHtmlUpdateInfo(
-										((Element) infoNode).getAttribute("ver"),
-										infoNode.getFirstChild().getNodeValue(),
-										((Element) infoNode).getAttribute("ver"));
-							}
-						}
-					}
-
-					if ("padweb_apk".equals(((Element) objNode).getAttribute("name"))) {
-						NodeList infoList = objNode.getChildNodes();
-						for (int j = 0; j < infoList.getLength(); j++) {
-							Node infoNode = infoList.item(j);
-							if ((objNode.getNodeType() == Node.ELEMENT_NODE)
-									&& ("old".equals(infoNode.getNodeName()) 
-											|| ("latest".equals(infoNode.getNodeName())))) {
-								updateInfo.addNewApkUpdateInfo(
-										((Element) infoNode).getAttribute("ver"),
-										infoNode.getFirstChild().getNodeValue());
-							}
-						}
-					}
-
-				}
-			}
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			updateInfo
+					.addNewHtmlUpdateInfo((((Element) (nodeList.item(i)))
+							.getAttribute("ver")), nodeList.item(i)
+							.getTextContent(), (((Element) (nodeList.item(i)))
+							.getAttribute("require_apk")));
 
 		}
-		if (inStream != null)
-			inStream.close();
+
+		nodeList = (NodeList) xpath
+				.evaluate(
+						"/objs/obj[@name=\"padweb_apk\"]/latest|/objs/obj[@name=\"padweb_apk\"]/old",
+						doc, XPathConstants.NODESET);
+		if (nodeList.getLength() == 0) {
+			String exception = "url parse failed:" + xmlText;
+			Log.v(TAG, exception);
+			throw new Exception(exception);
+		}
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			updateInfo.addNewApkUpdateInfo(
+					(((Element) (nodeList.item(i))).getAttribute("ver")),
+					nodeList.item(i).getTextContent());
+		}
+
 		return updateInfo;
 	}
 }
